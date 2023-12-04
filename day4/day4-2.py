e721a87987cd942b96e3a2cb13ac7a06f16ed1c8
Willy <152602217+willydrgl@users.noreply.github.com>
2023-12-04 22:33:11 +0100

    Jour 4-2

--- day4/day4-2.py
+++ day4/day4-2.py
@@ -1,5 +1,24 @@
 import re
 
+original_cards = []
+scratchcards = {}
+
+def process_card(i, card):
+    # Count number of winning numbers
+    nb_winning_numbers = 0
+    for number in card[1]:
+        if is_winning_number(number, card[0]):
+            nb_winning_numbers += 1
+    # Add cards to scratchcards total
+    for j in range(1, nb_winning_numbers + 1):
+        if i + j < len(original_cards):
+            if not i + j in scratchcards:
+                scratchcards[i+j] = 1
+            else:
+                scratchcards[i+j] += 1
+        else:
+            break
+
 def is_winning_number(number, winning_numbers):
     for n in winning_numbers:
         if number == n:
@@ -7,21 +26,28 @@ def is_winning_number(number, winning_numbers):
     return False
 
 def main():
-    total_points = 0
     with open("input.txt", "r", encoding='utf8') as file:
         for line in file:
-            card_points = 0
             winning_numbers = []
-            card = re.split(r'\s+', line)[2:]
-            for n in card[:card.index('|')]:
-                winning_numbers.append(n)
-            for n in card[card.index('|')+1:-1]:
-                if is_winning_number(n, winning_numbers):
-                    if card_points > 0:
-                        card_points = card_points * 2
-                    else:
-                        card_points = 1
-            total_points = total_points + card_points
-    print(total_points)
+            selected_numbers = []
+            split_card = re.split(r'\s+', line)[2:]
+            for number in split_card[:split_card.index('|')]:
+                winning_numbers.append(number)
+            for number in split_card[split_card.index('|')+1:-1]:
+                selected_numbers.append(number)
+            card = (winning_numbers, selected_numbers)
+            original_cards.append(card)
+    for i, card in enumerate(original_cards):
+        # Put original copy in scratchcards
+        if not i in scratchcards:
+            scratchcards[i] = 1
+        else:
+            scratchcards[i] += 1
+        for j in range(scratchcards[i]):
+            process_card(i, card)
+    total_scratchcards = 0
+    for i in scratchcards.values():
+        total_scratchcards += i
+    print(total_scratchcards)
 
 main()
