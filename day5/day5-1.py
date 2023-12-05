807c491aa85597a874a87a9bb70b8a76fa0b1fbe
Willy <152602217+willydrgl@users.noreply.github.com>
2023-12-05 18:41:26 +0100

    Add files via upload

--- day5/day5-1.py
+++ day5/day5-1.py
@@ -1,7 +1,13 @@
 import re
 
-def process_seeds(seeds, mapp):
-    print(mapp)
+def process_numbers(_numbers, _map):
+    new_numbers = _numbers
+    for i, n in enumerate(_numbers):
+        for a in _map:
+            if a[1] + a[2] > n >= a[1]:
+                new_numbers[i] = a[0] + (n - a[1])
+                break
+    return new_numbers
 
 def main():
 
@@ -16,6 +22,8 @@ def main():
 
     # Registering seeds and maps
     seeds = data[0][1]
+    for i in range(len(seeds)):
+        seeds[i] = int(seeds[i])
     data.pop(0)
 
     maps = []
@@ -23,11 +31,15 @@ def main():
         temp = []
         for i in range(int(len(array[1])/3)):
             temp.append(array[1][i*3:i*3+3])
+            for j in range(len(temp[i])):
+                temp[i][j] = int(temp[i][j])
         temp.sort(key=lambda x: x[0], reverse=True)
         maps.append(temp)
 
-    # Pass seeds through the maps
+    # Pass seeds through the maps to get the locations
+    numbers = seeds
     for array in maps:
-        process_seeds(seeds, array)
+        numbers = process_numbers(numbers, array)
+    print(min(numbers))
 
 main()
