5347b7d3b4798bdd4eebfe0b6cea892a437eb1e3
Willy <152602217+willydrgl@users.noreply.github.com>
2023-12-12 23:14:12 +0100

    Quoi de neuf

---
+++ day9/day9-2.py
@@ -0,0 +1,25 @@
+import re
+
+def predict_next_value(sequence):
+    i = 0
+    steps = []
+    while i < len(sequence) - 1:
+        steps.append(str(int(sequence[i+1]) - int(sequence[i])))
+        i = i + 1
+    if len(set(steps)) == 1 and steps[0] == '0':
+        return sequence[i]
+    next_value = str(int(sequence[i]) + int(predict_next_value(steps)))
+    return next_value
+
+def main():
+    data = []
+    with open("input.txt", 'r', encoding="UTF-8") as file:
+        for line in file:
+            data.append(re.findall(r'[\+\-0-9]+', line))
+    answer = 0
+    for e in data:
+        # e[::-1] only difference
+        answer += int(predict_next_value(e[::-1]))
+    print(answer)
+
+main()
